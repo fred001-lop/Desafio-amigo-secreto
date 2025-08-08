087e824984dda8c57bb2fb674559f8b415c03caa
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-08-07 19:17:40 -0600

    Se actuali el JS agregando la funcion

--- app.js
+++ app.js
@@ -3,3 +3,24 @@
 //Crear un array para almacenar los nombres
 let nombresSecretos = [];
 
+//Implementa una función para agregar amigos
+
+function agregarAmigo(){
+    //tomamos el valor del input con id=amigo
+    let input = document.getElementById('amigo');
+    let nombre = input.value.trim();// trim() quita espacios al inicio y final
+
+    //validamos que el campo no este vacio
+    if (nombre == ""){
+
+        alert("Por favor, inserte un nombre")
+        return;
+    }
+    //Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+    amigos.push(nombre);
+    console.log(`Lista de amigos: ${amigos}`);
+    //Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
+    input.value = "";
+
+
+}   
