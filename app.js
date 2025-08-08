d65b2e548fd6b9b1d089b110cd58d20872f0b145
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-08-07 21:56:40 -0600

    Se actualizo JS para limpiar antes de actualizar

--- app.js
+++ app.js
@@ -11,16 +11,22 @@ function agregarAmigo(){
     let nombre = input.value.trim();// trim() quita espacios al inicio y final
 
     //validamos que el campo no este vacio
-    if (nombre == ""){
+    if (nombre === ""){
 
-        alert("Por favor, inserte un nombre")
+        alert("Por favor, inserte un nombre");
         return;
     }
     //Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
-    amigos.push(nombre);
-    console.log(`Lista de amigos: ${amigos}`);
+    nombresSecretos.push(nombre);
+    console.log(`Lista de amigos: ${nombresSecretos}`);
     //Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
     input.value = "";
 
+    //Actualizar la lista visual en HTML
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';//limpiar lista antes de actualizar 
 
 }   
+
+console.log(agregarAmigo);
+console.log("nombre");
